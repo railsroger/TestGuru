17e97f531ef18593217ccfe8587464d87f708e31
railsroger <railsroger@gmail.com>
2018-11-23 18:23:59 +0300

    Fix association models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :user, foreign_key: :author_id
+  belongs_to :author, class_name: 'User'
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
