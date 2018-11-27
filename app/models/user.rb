8a6fae84f43767e61d9c6d3a31f71cf2f10a61f0
railsroger <railsroger@gmail.com>
2018-11-27 22:26:43 +0300

    Fix model user and schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
-  has_many :author_create, class_name: 'Test', foreign_key: :author_id
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
 
   def tests_level(level)
     tests.where(level: level)
