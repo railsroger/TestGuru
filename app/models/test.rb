0a3ef5cc7706bbc9498edb369eeaf5caf60905e7
railsroger <railsroger@gmail.com>
2018-11-27 23:59:15 +0300

    Fix code #2

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,11 +8,11 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..INFINITY) }
-  scope :return_tests, joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
+  scope :return_tests, -> (category) joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
   validates :level, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
   validates :title, :level, presence: true, uniqueness: true
 
-  def self.return_tests(category)
-    joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
-  end
+  #def self.return_tests(category)
+  #  joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
+  #end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
-  scope :tests_level, -> (level) { where(level: level) }
-
+  scope :tests_level, -> (level) { tests.where(level: level) }
   validates :name, :email, presence: true
 
-  def tests_level(level)
-    tests.where(level: level)
-  end
+  #def tests_level(level)
+  #  tests.where(level: level)
+  #end
 end
