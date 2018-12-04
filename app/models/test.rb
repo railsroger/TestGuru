cde3a991ad2af8be0e6e6c2f68989a5001b1d7fc
railsroger <railsroger@gmail.com>
2018-12-04 20:08:33 +0300

    Fix errors #3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,4 @@
 class Test < ApplicationRecord
-  INFINITY = Float::INFINITY
   belongs_to :category
   belongs_to :author, class_name: 'User'
   has_many :questions
@@ -7,13 +6,13 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   scope :easy, -> { where(level: 0..1) }
   scope :middle, -> { where(level: 2..4) }
-  scope :hard, -> { where(level: 5..INFINITY) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :by_category, -> (category) { joins(:category).where(categories: { title: category }) }
   validates :title, :level, presence: true
   validates :title, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
 
-  def self.return_tests(category)
+  def self.titles_by_category(category)
     Test.by_category(category).order(title: :desc).pluck(:title)
   end
 end
