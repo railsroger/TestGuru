411f11e9e225b68128d98c6f26d59d703ea4acbc
railsroger <railsroger@gmail.com>
2019-01-20 18:42:38 +0300

    Add localizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def default_url_options
-    { lang: I18n.locale }
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
   end
 
   private
