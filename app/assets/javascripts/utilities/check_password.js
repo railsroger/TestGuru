e654bc37599a2d26110d901fb1739bc937fbdf69
railsroger <railsroger@gmail.com>
2019-02-02 14:27:46 +0300

    Add checking password and length 6 digit

--- app/assets/javascripts/utilities/check_password.js
+++ app/assets/javascripts/utilities/check_password.js
@@ -1,10 +1,37 @@
-var check = function() {
-      if (document.getElementById('password').value ==
-          document.getElementById('password_confirmation').value) {
-          document.getElementById('message').style.color = 'green';
-          document.getElementById('message').innerHTML = 'matching';
-      } else {
-          document.getElementById('message').style.color = 'red';
-          document.getElementById('message').innerHTML = 'not matching';
-      }
+function checkPassword() {
+
+  var password = document.getElementById('password');
+  var password_confirmation = document.getElementById('password_confirmation');
+  var message = document.getElementById('message');
+  var goodColor = 'green';
+  var badColor = 'red';
+
+  if(password.value.length > 5) {
+
+    password.style.backgroundColor = goodColor
+    message.style.color = goodColor;
+    message.innerHTML = "Character number ok!"
+  }
+  else {
+
+    password.style.backgroundColor = badColor;
+    message.style.color = badColor;
+    message.innerHTML = "You have to enter at least 6 digit!"
+    return;
+  }
+
+  if(password.value == password_confirmation.value) {
+    
+    password_confirmation.style.backgroundColor = goodColor;
+    message.style.color = goodColor;
+    message.innerHTML = "matching!"
+  }
+
+  else {
+    
+    password_confirmation.style.backgroundColor = badColor;
+    message.style.color = badColor;
+    message.innerHTML = "These password don`t match!"
+  }
+
 }
