409f1ac363b1385e101cddd941c0f8b21ccf82ec
railsroger <railsroger@gmail.com>
2019-02-07 21:09:21 +0300

    Fix code

--- app/assets/javascripts/utilities/progress_bar.js
+++ app/assets/javascripts/utilities/progress_bar.js
@@ -1,12 +1,13 @@
 document.addEventListener('turbolinks:load', function() {
-    var progressBar = document.getElementById('progressbar');
+  let progressBar = document.querySelector('.progress-bar')
 
-    if (progressBar) {
-        var questionNumber = progressBar.dataset.questionNumber;
-        var questionsCount = progressBar.dataset.questionsCount;
+  if (progressBar) {
+    let currentQuestionIndex = progressBar.dataset.currentQuestionIndex
+    let questionsCount = progressBar.dataset.questionsCount
 
-        var progressPercent = questionNumber/questionsCount * 100;
-        
-        progressBar.style.width = progressPercent + "%";
-    }
-});
+    let percentage = (((currentQuestionIndex - 1) / questionsCount) * 100) + '%'
+
+    progressBar.style.width = percentage;
+    progressBar.textContent = percentage;
+  }
+})
