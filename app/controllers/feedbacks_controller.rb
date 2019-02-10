d522fb45378d4a3cfe3b645264704158524fbe32
railsroger <railsroger@gmail.com>
2019-02-10 21:43:10 +0300

    Create logic send feedback

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,23 @@
+class FeedbacksController < ApplicationController
+
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = current_user.feedbacks.new(feedback_params)
+
+    if @feedback.save
+      FeedbackMailer.send_feedback(@feedback).deliver_now
+      redirect_to root_path, alert: t('.success')
+    else
+      render :new, alert: t('.failed')
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:title, :body)
+  end
+end
---
+++ app/mailers/feedback_mailer.rb
@@ -0,0 +1,10 @@
+class FeedbackMailer < Application
+
+  def send_feedback(feedback)
+    @user = feedback.user
+    @subject = feedback.title
+    @message = feedback.body
+
+    mail to: Admin.first.email, from: @user.email, subject: 'Feedback TestGuru'
+  end
+end
