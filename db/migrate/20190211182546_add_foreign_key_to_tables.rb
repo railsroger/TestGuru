ce8b2a4b315f2b18b9fb62dae59fc21b688b6524
railsroger <railsroger@gmail.com>
2019-02-11 22:23:10 +0300

    Fix

---
+++ db/migrate/20190211182546_add_foreign_key_to_tables.rb
@@ -0,0 +1,11 @@
+class AddForeignKeyToTables < ActiveRecord::Migration[5.2]
+  def change
+    add_foreign_key :tests, :users, column: :author_id
+    add_foreign_key :answers, :questions
+    add_foreign_key :gists, :questions
+    add_foreign_key :gists, :users
+    add_foreign_key :questions, :tests
+    add_foreign_key :test_passages, :tests
+    add_foreign_key :test_passages, :users 
+  end
+end
